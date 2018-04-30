e9efeabc4e7b16a2dc93a031c9b6a68f30f40958
DavidEugenePratt <davidpratt@foxhead.com>
2018-04-30 15:16:08 -0700

    Adds node packages to install

--- recipes/node.rb
+++ recipes/node.rb
@@ -4,21 +4,25 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-bash 'install nvm' do
-  users = []
 
-  if node.read 'meanbox', 'node', 'users'
-    users = node['meanbox']['node']['users']
-  else
-    node['etc']['passwd'].each do |systemuser, _data|
-      users.push(systemuser) if Dir.exist? '/home/' + systemuser
-    end
+users = []
+
+if node.read 'meanbox', 'node', 'users'
+  users = node['meanbox']['node']['users']
+else
+  node['etc']['passwd'].each do |systemuser, _data|
+    users.push(systemuser) if Dir.exist? '/home/' + systemuser
   end
+end
 
-  nodeversion = node.read('meanbox', 'node', 'version') ? node['meanbox']['node']['version'] : 'node'
+packages = node.read('meanbox', 'node', 'packages') ? node['meanbox']['node']['packages'] : []
 
-  users.each do |username, _data|
-    next unless Dir.exist? '/home/' + username
+nodeversion = node.read('meanbox', 'node', 'version') ? node['meanbox']['node']['version'] : 'node'
+
+users.each do |username, data|
+  next unless Dir.exist? '/home/' + username
+
+  bash 'install nvm' do
 
     user username
 
@@ -27,10 +31,22 @@ bash 'install nvm' do
     environment('HOME' => ::Dir.home(username), 'USER' => username)
 
     code <<-EOH
-
       curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
       export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install #{nodeversion} && nvm use #{nodeversion}
-
     EOH
   end
+
+  packages.each do |package, _data|
+    packageversion = package["version"] ? "@#{package["version"]}" : ''
+
+    bash "install #{package['name']}" do
+
+      user username
+      environment('HOME' => ::Dir.home(username), 'USER' => username)
+      code <<-EOH
+        export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && npm install -g #{package["name"]}#{packageversion}
+      EOH
+    end
+  end
+
 end
